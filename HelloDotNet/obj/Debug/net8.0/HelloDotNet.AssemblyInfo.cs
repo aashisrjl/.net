f626f0ff5adf9a21d41aa45a84e0890d505c16b7
aashisrjl <aashis252@gmail.com>
2025-06-29 07:53:12 +0545

    LING and lamda

--- HelloDotNet/obj/Debug/net8.0/HelloDotNet.AssemblyInfo.cs
+++ HelloDotNet/obj/Debug/net8.0/HelloDotNet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloDotNet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b400e46bc5689fb877310c30571e06359c1fe02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d0e97f797059e20f11de2828eed040c387908ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloDotNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloDotNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
